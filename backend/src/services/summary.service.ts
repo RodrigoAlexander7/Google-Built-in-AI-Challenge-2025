7b1dbf55f6f3555b23640940146f59d299c2082f
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-15 15:15:18 -0500

    Adding explain and sumarice service

---
+++ backend/src/services/summary.service.ts
@@ -0,0 +1,18 @@
+import { geminiApi } from "@/configs/gemini.configs";
+
+export class SummaryService {
+   async summarizeText(prompt: string) {
+      const response = await geminiApi.models.generateContent({
+         model: "gemini-2.5-flash",
+         contents: `Sumarize this text ${prompt}`,
+      });
+      return response.text
+   }
+   async explainText(prompt: string) {
+      const response = await geminiApi.models.generateContent({
+         model: "gemini-2.5-flash",
+         contents: `Explain this: ${prompt}`,
+      });
+      return response.text
+   }
+}
