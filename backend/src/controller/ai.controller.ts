43b1c90328f2ee0555b8d4c93874d1aef43afdf2
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-15 16:49:16 -0500

    fear: addign routes to the gemini api

--- backend/src/controller/ai.controller.ts
+++ backend/src/controller/ai.controller.ts
@@ -4,14 +4,30 @@ import { AIService } from "@/services/ai.service";
 export class AIController {
    private aiService = new AIService();
 
-   async summarizeText(req: Request, res: Response, next?: NextFunction) {
+   summarizeText = async (req: Request, res: Response, next: NextFunction) => {
       try {
          const { text } = req.body;
          if (!text) return res.status(400).json({ error: 'No text to summarize' })
-         const summary = this.aiService.summarizeText(text)
+         // IMPORTANT: await
+         const summary = await this.aiService.summarizeText(text)
+
          res.json({ summary })
       } catch (error) {
-         //next(error)
+         next(error)
+      }
+   }
+
+
+   explainText = async (req: Request, res: Response, next: NextFunction) => {
+      try {
+         const { text } = req.body;
+         if (!text) return res.status(400).json({ error: 'No text to summarize' })
+
+         const explanation = await this.aiService.explainText(text)
+
+         res.json({ explanation })
+      } catch (error) {
+         next(error)
       }
    }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv"
 import authRouter from '@/auth/auth.route'
 import { verifyToken } from "@/middleware/auth";
+import aiRouter from "@/routes/ai.routes";
+import { errorHandler } from "@/middleware/error.middleware";
 
 dotenv.config()   // load environment var
 const port = 3000;
@@ -14,6 +16,11 @@ app.get("/", verifyToken, (req: Request, res: Response) => {
    res.send("Hello word xd");
 });
 
+app.use('/ai', aiRouter)  // AI routes
+
+app.use(errorHandler)
+
 app.listen(port, () => {
    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
