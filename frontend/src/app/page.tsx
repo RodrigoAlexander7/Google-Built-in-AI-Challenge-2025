3b63f42237dc22a5fe2155f98c3431e8dc635d2a
dev1d123 <dhuamanio@unsa.edu.pe>
2025-10-18 23:21:43 -0500

    refactor: navbar and template setting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { useState } from 'react';
-import Summarizer from "./pages/Summarizer"
+import Template from "./pages/Template"
 
 export default function Home() {
   
   return (
     <div className="min-h-screen bg-gray-50">
-      <Summarizer/>
+      <Template/>
     </div>
   );
 }
