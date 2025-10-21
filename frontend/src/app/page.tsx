741d07a9afc105d1ec51c54ca57d433bd63faa88
dev1d123 <dhuamanio@unsa.edu.pe>
2025-10-21 09:03:47 -0500

    added: flashcard component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,11 @@
 
 import Template from './pages/Template';
 import Practice from './pages/Practice';
-
+import FlashCard from './pages/FlashCard';
 export default function Home() {
   return (
     <Template>
-      <Practice />
+      <FlashCard />
     </Template>
   );
 }
