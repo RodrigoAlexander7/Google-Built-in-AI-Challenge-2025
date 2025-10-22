8bbacddfee52e6e330b533f12bb9b012ab5efa32
dev1d123 <dhuamanio@unsa.edu.pe>
2025-10-22 05:44:48 -0500

    fix: all routing

--- frontend/src/app/summarizer/SummarizerPage.tsx
+++ frontend/src/app/summarizer/SummarizerPage.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import SummaryOptions, { SummaryOptionsData } from '../components/layout/SummaryOptions';
 import PromptInput from '../components/layout/PromptInput';
