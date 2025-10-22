19e224d092335fab9277bfed7b161ac0b98cd618
dev1d123 <dhuamanio@unsa.edu.pe>
2025-10-22 05:37:23 -0500

    fixed: route for learnplay

---
+++ frontend/src/app/learn-play/page.tsx
@@ -0,0 +1,9 @@
+import Template from "../pages/Template";
+import LearnPlayPage from "./LearnPlayPage";
+export default function Home() {
+    return (
+        <Template>
+            <LearnPlayPage></LearnPlayPage>
+        </Template>
+    );
+} 
